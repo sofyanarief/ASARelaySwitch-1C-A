628acbfdfa5e9a28771906c9e1c5fa3db5c0370d
Sofyan Arief <sofyanarief@Sofyans-Mac-Pro.local>
2024-12-27 23:50:11 +0700

    remove mqtt publish method

--- mqtt_listener.py
+++ mqtt_listener.py
@@ -1,3 +1,4 @@
+import machine
 from time import sleep
 from umqtt_simple import MQTTClient
 from config_manager import ConfigManager
@@ -9,31 +10,6 @@ class MQTTListener:
        self.configDict = configManagerObj.readConfig()
        self.topicsToSubs = []
        self.relayListenerObj = RelayListener()
-    
-    def publishMessage(self, topic, message):
-        client = MQTTClient(self.configDict.get('deviceName'), self.configDict.get('mqttServer'), 1883)
-        try:
-            print('Connecting to MQTT Server')
-            client.connect()
-        except:
-            print('Can\'t connect to MQTT Server')
-        else:
-            print('Connected to MQTT Server')
-            numRetry = 1
-            while numRetry <=3:
-                try:
-                    print('Trying to publish message to MQTT Server for '+str(numRetry)+' time')
-                    client.publish(topic, message)
-                except:
-                    print('Can\'t publish to MQTT Server')
-                    numRetry += 1
-                    sleep(1)
-                else:
-                    print('Message published on MQTT server')
-                    break
-            else:
-                print('Can\'t publish to MQTT Server after 3 times retry')
-            client.disconnect()
 
     def mqttCallback(self, topic, msg):
         topicStr = topic.decode('utf-8')
@@ -80,4 +56,4 @@ class MQTTListener:
                 sleep(1)
         except OSError as e:
             print('Failed to connect:', e)
-
+            machine.reset()
